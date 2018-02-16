00d0281f01547643b3e3e6ccb9a7f100a04df4bb
RybakTaras <rybaktarasw@gmail.com>
2018-02-16 16:29:23 +0200

    added menu btn, header-content

--- gulp_template/dev/js/common.js
+++ gulp_template/dev/js/common.js
@@ -2,5 +2,10 @@
 // svg4everybody({});
 
 $(document).ready(function(){
+  // MENU BUTTON
+    $('.header-nav__btn').click(function(){
+      $('.header-nav__svg').toggleClass('open');
+    })
+  // WOW JS
     new WOW().init();
 });
